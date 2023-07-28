98e3ea5ab4aa712fac489e39baa455cb401a3ffa
Daniel100001 <danldplayer2gmail.com>
2023-07-28 12:41:52 +0600

    Added ViewPager And TabLayout

--- app/src/main/java/com/example/android5kitsuapiteamwork/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/android5kitsuapiteamwork/ui/fragments/home/HomeFragment.kt
@@ -1,19 +1,30 @@
 package com.example.android5kitsuapiteamwork.ui.fragments.home
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.android5kitsuapiteamwork.R
+import com.example.android5kitsuapiteamwork.base.BaseFragment
+import com.example.android5kitsuapiteamwork.databinding.FragmentHomeBinding
+import com.example.android5kitsuapiteamwork.ui.adapters.AnimeViewPager
+import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment() {
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override val binding by viewBinding(FragmentHomeBinding::bind)
+    override val viewModel: HomeViewModel by viewModels()
+
+    private val fragListNames = listOf(
+        "Anime",
+        "Manga"
+    )
+
+    override fun initialize() {
+        val adapter = AnimeViewPager(this)
+        binding.viewPager.adapter = adapter
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, pos ->
+            tab.text = fragListNames[pos]
+        }.attach()
     }
 }
